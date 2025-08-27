91d61421735f7735207c0182d4f67ed17f8677ca
nachyocheese908 <kidclarkes@gmail.com>
2025-08-27 11:46:41 -0600

    Fixed all bugs and fleshed out the progam to not repeat ages and if one remains, it must be the users age

--- ageguess.py
+++ ageguess.py
@@ -1,13 +1,18 @@
 import random
 def main():
-    name = input("Hello! My program will try and guess your Age!\nWhat is your name?: ")
-    age = random.randrange(18,31)
-    response = input(f'Is your age {age}?[y/n]: ')
+    name = input('Hello! My program will guess your age!\nWhat is your name?: ')
+    ages = list(range(16,31))
+    age = random.choice(ages)
+    response = None
     while response != 'y':
-        print('Rats...')
-        age = random.randrange(15,31)
+        if len(ages) == 1:
+            print(f'Your age must be {ages[0]}!\nYour name is {name} and you are {age} years old!')
+            return None    
         response  = input(f'Is your age {age}?[y/n]: ')
-    print(f'Your name is {name} and you are {response} years old!')
+        ages.remove(age)
+        print('Rats...')
+        age = random.choice(ages)
+    print(f'Your name is {name} and you are {age} years old!')
     return None
 
 main()
