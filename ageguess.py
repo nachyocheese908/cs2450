36aec14146ba169ba8ea3d786db89e74351eec5f
nachyocheese908 <kidclarkes@gmail.com>
2025-08-27 11:27:16 -0600

    first commit

---
+++ ageguess.py
@@ -0,0 +1,13 @@
+import random
+def main():
+    name = input("Hello! My program will try and guess your Age!\nWhat is your name?: ")
+    age = random.randrange(15,31)
+    response = input(f'Is your age {age}?[y/n]: ')
+    while response != 'y':
+        print('Rats...')
+        age = random.randrange(15,31)
+        response  = input(f'Is your age {age}?[y/n]: ')
+    print(f'Your name is {age} and you are {response} years old!')
+    return None
+
+main()
